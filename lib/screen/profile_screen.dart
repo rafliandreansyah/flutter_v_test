97c7d8af79e371dc1f5c257bb43f6cd808946046
rafli <rafli@mediainovasi.id>
2023-11-30 17:12:46 +0700

    profile on progress

--- lib/screen/profile_screen.dart
+++ lib/screen/profile_screen.dart
@@ -5,9 +5,37 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Screen,',
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withAlpha(
+                10,
+              ),
+              spreadRadius:
+                  0, //also play with this two values to achieve your ideal result
+              blurRadius: 8,
+              offset: const Offset(
+                0,
+                8,
+              ),
+            ),
+          ]),
+      child: Column(
+        children: [
+          Container(
+            width: 320,
+            height: 170,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage('assets/images/img_bg_card_profile.png'),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
